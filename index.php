97ccb1eddb2f406495abc23b83a1a6e3184bb1c4
Sonu Kumar <sonukumar@deltaneev.local>
2021-03-08 17:47:27 +0530

    Update work

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
-<?php
-    echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DEV</title>
+</head>
+<body>
+    <!-- html comment -->
+    <h1>Learning PHP Programming</h1>
+    <br/>
+    <?php
+    // Single line comment in PHP
+    /* 
+        multiple line comment in php
+    */ 
+    echo "Lets start learning PHP";
+    echo '<br/>';
+    echo "Keep Going, Keep Going!";
+    echo '<br/>';
+    ?>
+
+    <?php 
+     
+        // declare variable
+        $name = 'RK Varun';
+        $age = 22;
+
+        //echo variable
+        echo $name;
+        echo "<h1>My Name is: ".$name. ' </h1>';
+        echo "<h1>My Age is: ".$age. ' </h1>';
+    ?>
+
+</body>
+</html>
